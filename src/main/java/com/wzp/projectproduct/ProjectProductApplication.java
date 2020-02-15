14ac18673b66a7e6af24b5529aca02f2a514d8db
wangziping <wangziping@whaty.com>
2020-02-15 20:26:32 +0800

    修改密码

--- src/main/java/com/wzp/projectproduct/ProjectProductApplication.java
+++ src/main/java/com/wzp/projectproduct/ProjectProductApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -19,6 +19,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan({"com.wzp.module.*.mapper"})
 @ComponentScan("com.wzp")
 @EnableTransactionManagement // 开启事务
+@EnableScheduling // 开启定时任务
 @SpringBootApplication
 public class ProjectProductApplication extends SpringBootServletInitializer {
 
